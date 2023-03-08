250c3170721c1a9fc25a88115d5ee934562104dc
MDegano <123469601+MDeganoWDev@users.noreply.github.com>
2023-03-08 15:54:10 +0100

    twik exo5

---
+++ Udemy-next/Exo5/pages/[pid].js
@@ -0,0 +1,47 @@
+import path from "path";
+import fs from "fs/promises";
+import React from "react";
+
+const ProductDetailPage = (props) => {
+  const { loadedProduct } = props;
+
+  if (!loadedProduct){
+     return <p>Loading...</p>
+  }
+
+  return (
+    <>
+      <h1>{loadedProduct.title}</h1>
+      <p>{loadedProduct.description}</p>
+    </>
+  );
+};
+
+export async function getStaticProps(context) {
+  const { params } = context;
+
+  const productId = params.pid;
+
+  const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+
+  const product = data.products.find((product) => product.id === productId);
+
+  return {
+    props: {
+      loadedProduct: product,
+    },
+  };
+}
+
+export async function getStaticPaths() {
+  return {
+    paths: [
+      { params: { pid: "p1" } },
+    ],
+    fallback: true
+  };
+}
+
+export default ProductDetailPage;
